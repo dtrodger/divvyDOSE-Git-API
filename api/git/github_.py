303680ae5ea0acd51fdd740ccbece28e638fb278
“dtrodger” <dtrodger20@gmail.com>
2021-04-28 15:06:07 -0700

    reduce github client helpers

--- api/git/github_.py
+++ api/git/github_.py
@@ -5,11 +5,7 @@ GitHub REST client
 """
 
 from flask import current_app
-from github import (
-    Github,
-    Organization,
-    Repository
-)
+from github import Github
 
 
 github_client = None
@@ -27,75 +23,27 @@ def get_github_client(reset:bool = False) -> Github:
     return github_client
 
 
-def github_organization(github_client: Github, organization: str) -> Organization:
-    """
-    Gets a GitHub organization
-    """
-
-    return github_client.get_organization(organization)
-
-
-def github_repositories(organization: Organization) -> list:
-    """
-    Gets a GitHub organizations repositories
-    """
-
-    return organization.get_repos()
-
-
-def github_repository_forks(repository: Repository) -> int:
-    """
-    Gets a count of GitHub repository forks
-    """
-
-    return repository.forks_count
-
-
-def github_repository_language(repository: Repository) -> str:
-    """
-    Gets a repositories language
-    """
-
-    return repository.language
-
-
-def github_repository_watchers(repository: Repository) -> list:
-    """
-    Gets a repositories watchers
-    """
-
-    return repository.watchers_count
-
-
-def github_repository_topics(repository: Repository) -> list:
-    """
-    Gets a repositories topics
-    """
-
-    return repository.get_topics()
-
-
 def github_organization_profile(organization_name: str) -> dict:
     """
     Builds a GitHub organization's profile
     """
 
     github_client = get_github_client()
-    organization = github_organization(github_client, organization_name)
+    organization = github_client.get_organization(organization_name)
     organization_public_repository_count = 0
     organization_forked_repository_count = 0
     organization_watchers_count = 0
     organization_languages = set()
     organization_topics = set()
-    for repository in github_repositories(organization):
+    for repository in organization.get_repos():
         organization_public_repository_count += 1
-        organization_forked_repository_count += github_repository_forks(repository)
-        organization_watchers_count += github_repository_watchers(repository)
+        organization_forked_repository_count += repository.forks_count
+        organization_watchers_count += repository.watchers_count
         repository_language = repository.language
         if repository_language:
             organization_languages.add(repository_language)
 
-        repository_topics = github_repository_topics(repository)
+        repository_topics = repository.get_topics()
         if repository_topics:
             organization_topics.update(repository_topics)
 
